efa9fde58b9ceac63152cc7ccd870f33ab9cabe3
sounstudio@hotmail.com <Nosepuede19845>
2022-03-01 11:33:27 +0100

    funcionando los disparos y visualizacion de los escudos

---
+++ core/src/actor/EnemyShip.java
@@ -0,0 +1,42 @@
+package actor;
+
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+public class EnemyShip extends Ship{
+
+
+    public EnemyShip(float xCentre, float yCentre, float width,
+                      float height, float movementSpeed, int shield, float laserWidth,
+                      float laserHeight, float laserMovementSpeed, float timeBetweenShot,
+                      TextureRegion shipTextureRegion,
+                      TextureRegion shieldTextureRegion,
+                      TextureRegion laserTextureRegion) {
+        super(xCentre, yCentre, width, height, movementSpeed, shield, laserWidth, laserHeight, laserMovementSpeed, timeBetweenShot, shipTextureRegion, shieldTextureRegion, laserTextureRegion);
+    }
+
+    @Override
+    public Laser[] fireLaser() {
+        Laser[]laser = new Laser[2];
+        laser[0] = new Laser(boundingBox.x + boundingBox.width * 0.18f,boundingBox.y - laserHeight,
+                laserWidth,laserHeight,
+                laserMovementSpeed, laserTextureRegion);
+        laser[1] = new Laser(boundingBox.x + boundingBox.width * 0.82f,boundingBox.y - laserHeight,
+                laserWidth,laserHeight,
+                laserMovementSpeed, laserTextureRegion);
+
+        timeSinceLastShop = 0;
+
+        return laser;
+    }
+
+    @Override
+    public void draw(Batch batch){
+        batch.draw(shipTextureRegion,boundingBox.x,boundingBox.y,boundingBox.width,boundingBox.height);
+        if (shield > 0){
+            batch.draw(shieldTextureRegion,boundingBox.x,boundingBox.y -boundingBox.height *0.2f,boundingBox.width,boundingBox.height);
+
+
+        }
+    }
+}
---
+++ core/src/actor/PlayerShip.java
@@ -0,0 +1,31 @@
+package actor;
+
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+public class PlayerShip extends Ship{
+
+
+    public PlayerShip(float xCentre, float yCentre, float width,
+                      float height, float movementSpeed, int shield, float laserWidth,
+                      float laserHeight, float laserMovementSpeed, float timeBetweenShot,
+                      TextureRegion shipTextureRegion,
+                      TextureRegion shieldTextureRegion,
+                      TextureRegion laserTextureRegion) {
+        super(xCentre, yCentre, width, height, movementSpeed, shield, laserWidth, laserHeight, laserMovementSpeed, timeBetweenShot, shipTextureRegion, shieldTextureRegion, laserTextureRegion);
+    }
+
+    @Override
+    public Laser[] fireLaser() {
+        Laser[]laser = new Laser[2];
+        laser[0] = new Laser(boundingBox.x + boundingBox.width * 0.07f,boundingBox.y + boundingBox.height * 0.45f,
+                laserWidth,laserHeight,
+                laserMovementSpeed, laserTextureRegion);
+        laser[1] = new Laser(boundingBox.x + boundingBox.width * 0.93f,boundingBox.y + boundingBox.height * 0.45f,
+                laserWidth,laserHeight,
+                laserMovementSpeed, laserTextureRegion);
+
+        timeSinceLastShop = 0;
+
+        return laser;
+    }
+}
